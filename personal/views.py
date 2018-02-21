8fb947c81059392f6e7a316b1e36283438c6729c
Shivam Shukla <shivama98@gmail.com>
2018-02-21 20:21:59 +0530

    First Commit

---
+++ personal/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html',{'content':['If, you would like to contact me, please email me.','shivama98@gmail.com']})
